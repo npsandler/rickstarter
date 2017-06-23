d0f3fba6e87a65dc49fab39dec54b3343cc62581
Nathaniel Sandler <npsandler@gmail.com>
2017-06-23 13:46:38 -0400

    work on date remaining display

--- app/views/api/projects/_project.json.jbuilder
+++ app/views/api/projects/_project.json.jbuilder
@@ -1,3 +1,3 @@
-json.extract! project, :id, :title, :end_date, :funding_goal, :details, :creator, :category, :description
+json.extract! project, :id, :title, :end_date, :funding_goal, :details, :creator, :category, :description, :current_funding
 
 # json.end_date project.end_date.to_time
--- frontend/components/projects/show_page.jsx
+++ frontend/components/projects/show_page.jsx
@@ -9,10 +9,20 @@ class ShowPage extends React.Component {
 
   componentDidMount() {
     if (this.props.project === undefined) {
-      this.props.requestSingleProject(this.props.match.params.projectId)
+      this.props.requestSingleProject(this.props.match.params.projectId);
     }
   }
 
+  numberWithCommas(num) {
+    num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  dateRemaining() {
+    const currentDate = new Date();
+    return this.numberWithCommas(
+      Math.ceil((new Date(this.props.end_date) - currentDate) / 86400000)
+    );
+  }
 
   render() {
     // TODO: renderuserimage properly
@@ -20,13 +30,13 @@ class ShowPage extends React.Component {
     // TODO: rewards view
     const { project } = this.props;
     if (project) {
-      debugger
+      // check project.current_funding
       return   (
         <article className='show-background'>
           <section className='headline content-box'>
             <section className='content-subsection'>
               <section className='creator-area'>
-                <div className='creator-img'></div>
+                <div className='creator-img'> prof-pic</div>
                 <div className='creator-name'>{project.creator.name}</div>
               </section>
               <section className='headline-text'>
@@ -41,11 +51,11 @@ class ShowPage extends React.Component {
               </section>
               <ul className='project-stats'>
                 <li className='current-funding-text'>${project.current_funding}</li>
-                <li className='funding-goal-text'>pledged of a ${project.current_funding} goal</li>
+                <li className='funding-goal-text'>pledged of a ${project.funding_goal} goal</li>
                 <li className='stat'></li>
-                <li className='stat-line'></li>
-                <li className='stat'></li>
-                <li className='stat-line'></li>
+                <li className='stat-line'>backers</li>
+                <li className='stat'>{this.dateRemaining()}</li>
+                <li className='stat-line'>days to go</li>
                 <li>
                   <button className='backer-button'>Back this project</button>
                 </li>
