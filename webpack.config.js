ebd23cf254e3f3fe54dbcf0a7a4878fccff1f859
Nathaniel Sandler <npsandler@gmail.com>
2017-07-13 10:51:45 -0400

    add configurations to fix console error

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,18 @@ module.exports = {
   devtool: 'inline-source-map',
   resolve: {
     extensions: [".js", ".jsx", "*"]
-  }
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
+
 };
