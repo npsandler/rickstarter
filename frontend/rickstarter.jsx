9978aadfc5a9ed60f9f52c83bf1607195b499bc0
Nathaniel Sandler <npsandler@gmail.com>
2017-08-04 16:20:00 -0400

    remove a few comments

--- frontend/rickstarter.jsx
+++ frontend/rickstarter.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// window.signUp = APIUtil.signUp;
-// window.logIn = APIUtil.logIn;
-// window.logOut = APIUtil.logOut;
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
